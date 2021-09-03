141a7aa2a41a276c3297260181670ed976d289e1
Mikkel Stolborg <mikkel.stolborg@capgemini.com>
2021-09-03 11:32:46 +0200

    First commit with simple working command line

---
+++ DrinkChocolate/PersistantData.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace DrinkChocolate
+{
+    class PersistantData
+    {
+        public int MaxDrinks { get; set; }
+
+        [XmlArray(ElementName ="DrinkList")]
+        public List<DateTime> DrinkList { get; set; }
+
+        // create temp undo and redo list.
+        // should be persisted as long as the program is running
+        private List<DateTime> TempItemList { get; set; }
+
+        public PersistantData()
+        {
+            DrinkList = new List<DateTime>();
+            TempItemList = new List<DateTime>();
+        }
+
+        private DateTime GetLastDrinkTime()
+        {
+            var date = DrinkList.Count == 0? DateTime.MinValue : DrinkList.Last();
+
+            return date;
+        }
+
+        public string GetTimneSinceLastDrink()
+        {
+            var lastDrinkDate = GetLastDrinkTime();
+            if(lastDrinkDate == DateTime.MinValue)
+            {
+                return "For evigt";
+            }
+            var time = DateTime.Now.Subtract(lastDrinkDate);
+            return time.ToString(@"hh\:mm\:ss");
+        }
+
+        public void ClearList()
+        {
+            DrinkList = new List<DateTime>();
+            // save the data, to clear persisted data.
+            SaveData();
+        }
+
+        public void AddDrink()
+        {
+            DrinkList.Add(DateTime.Now);
+            // Auto save data for persistency
+            SaveData();
+        }
+
+        public void SaveData()
+        {
+            // serialize data and save it.
+            XmlSerializer aSerializer = new XmlSerializer(typeof(List<DateTime>));
+            StringBuilder sb = new StringBuilder();
+            StringWriter sw = new StringWriter(sb);
+            aSerializer.Serialize(sw, DrinkList);
+            string xmlResult = sw.GetStringBuilder().ToString();
+
+            // write the xml to a file
+            File.WriteAllText(AppContext.BaseDirectory + @"\saveData.txt", xmlResult,Encoding.UTF8);
+        }
+
+        public void UndoLastAdd()
+        {
+            // check size 
+            if(DrinkList.Count < 1)
+            {
+                // Nothing to undo
+                return;
+            }
+            var lastDateTime = DrinkList.Last();
+            TempItemList.Add(lastDateTime);
+            DrinkList.RemoveAt(DrinkList.Count - 1);
+        }
+
+        public void RedoLastAdd()
+        {
+            // check temp size
+            if(TempItemList.Count < 1)
+            {
+                // no action to redo
+                return;
+            }
+            var redoAdd = TempItemList.Last();
+            DrinkList.Add(redoAdd);
+            TempItemList.RemoveAt(TempItemList.Count - 1);
+        }
+
+        public void LoadData()
+        {
+            // Create an instance of the XmlSerializer.
+            XmlSerializer serializer =
+            new XmlSerializer(typeof(List<DateTime>));
+
+            // Declare an object variable of the type to be deserialized.
+
+            using (StreamReader reader = new StreamReader(AppContext.BaseDirectory + @"\saveData.txt",Encoding.UTF8))
+            {
+                // Call the Deserialize method to restore the object's state.
+                DrinkList = (List<DateTime>)serializer.Deserialize(reader);
+            }
+        }
+    }
+}
---
+++ DrinkChocolate/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DrinkChocolate
+{
+    class Program
+    {
+
+        private static PersistantData Data { get; set; }
+        static void Main(string[] args)
+        {
+            Data = new PersistantData();
+            Data.LoadData();
+
+            // how many drinks can i have per week?
+            Data.MaxDrinks = 5;
+
+            //Simulate running program
+            bool running = true;
+            while (running)
+            {
+                var line = Console.ReadLine();
+
+                switch (line)
+                {
+                    case "q":
+                        running = false;
+                        break;
+                    case "d":
+                        CanDrink(Data.MaxDrinks);
+                        break;
+                    case "a":
+                        Data.AddDrink();
+                        Console.WriteLine("Added one drnk");
+                        break;
+                    case "w":
+                        // get time since last drink
+                        Console.WriteLine(Data.GetTimneSinceLastDrink());
+                        break;
+                    case "c":
+                        Data.ClearList();
+                        Console.WriteLine("Cleared Drinks");
+                        break;
+                    case "u":
+                        // undo last addition
+                        Data.UndoLastAdd();
+                        break;
+                    case "r":
+                        Data.RedoLastAdd();
+                        break;
+                    case "count":
+                        // print how many you have been drinking
+                        Console.WriteLine(Data.DrinkList.Count);
+                        break;
+                    default:
+                        break;
+                }
+
+                if (line.Contains("set"))
+                {
+                    var parts = line.Split(" ");
+                    int.TryParse(parts[1], out int newMax);
+                    Data.MaxDrinks = newMax;
+                }
+            }
+        }
+
+        private static void CanDrink(int numDrinks)
+        {
+            // make a randomizer that gives either a yes or no.
+            // the closer you get to the limit, the more no you get.
+            Random ran = new Random();
+
+            int ranRange = ran.Next(100) + 1;
+
+            float multiplier = 100 / numDrinks;
+
+            float noRange = multiplier * Data.DrinkList.Count;
+
+            if (ranRange > noRange)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
